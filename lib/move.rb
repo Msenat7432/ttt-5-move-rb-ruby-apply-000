81cb558ef358673d0bbb383f5bc6d9a272d03bfb
Max Senat <m.senat@yahoo.com>
2020-10-11 15:49:33 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,6 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move (board, input_to_index,first_player = "x")
+def move (board, input_to_index,first_player = "X")
   board[input_to_index] = first_player
 end
